0dfb082a1794da3162dec1004ce34239d6360c98
PikaChien <ff12312323@gmail.com>
2017-03-06 18:59:07 +0800

    live sco and record to wave

--- app/src/main/java/example/com/bluetothrecord/MainActivity.java
+++ app/src/main/java/example/com/bluetothrecord/MainActivity.java
@@ -1,6 +1,7 @@
 package example.com.bluetothrecord;
 
 import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -23,185 +24,31 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Environment;
 import android.util.Log;
-import java.lang.CharSequence;
-import java.io.IOException;
 import android.widget.Toast;
 import android.Manifest;
 import android.support.annotation.NonNull;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+
+import java.lang.CharSequence;
+import java.io.IOException;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String LOG_TAG = "AudioRecordTest";
-    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
-    private static String mFileName = null;
-
-    private RecordButton mRecordButton = null;
-    private MediaRecorder mRecorder = null;
-
-    private PlayButton   mPlayButton = null;
-    private MediaPlayer   mPlayer = null;
-
-    // Requesting permission to RECORD_AUDIO
-    private boolean permissionToRecordAccepted = false;
-    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode){
-            case REQUEST_RECORD_AUDIO_PERMISSION:
-                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                break;
-        }
-        if (!permissionToRecordAccepted ) finish();
-
-    }
-
-    private void onRecord(boolean start) {
-        if (start) {
-            startRecording();
-        } else {
-            stopRecording();
-        }
-    }
-
-    private void onPlay(boolean start) {
-        if (start) {
-            startPlaying();
-        } else {
-            stopPlaying();
-        }
-    }
-
-    private void startPlaying() {
-        mPlayer = new MediaPlayer();
-        try {
-            mPlayer.setDataSource(mFileName);
-            mPlayer.prepare();
-            mPlayer.start();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
-        }
-    }
-
-    private void stopPlaying() {
-        mPlayer.release();
-        mPlayer = null;
-    }
-
-    private void startRecording() {
-        mRecorder = new MediaRecorder();
-        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mRecorder.setOutputFile(mFileName);
-        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-
-        try {
-            mRecorder.prepare();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
-        }
-
-        mRecorder.start();
-    }
-
-    private void stopRecording() {
-        mRecorder.stop();
-        mRecorder.release();
-        mRecorder = null;
-    }
-
-    class RecordButton extends Button {
-        boolean mStartRecording = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onRecord(mStartRecording);
-                if (mStartRecording) {
-                    setText("Stop recording");
-                } else {
-                    setText("Start recording");
-                }
-                mStartRecording = !mStartRecording;
-            }
-        };
-
-        public RecordButton(Context ctx) {
-            super(ctx);
-            setText("Start recording");
-            setOnClickListener(clicker);
-        }
-    }
-
-    class PlayButton extends Button {
-        boolean mStartPlaying = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onPlay(mStartPlaying);
-                if (mStartPlaying) {
-                    setText("Stop playing");
-                } else {
-                    setText("Start playing");
-                }
-                mStartPlaying = !mStartPlaying;
-            }
-        };
-
-        public PlayButton(Context ctx) {
-            super(ctx);
-            setText("Start playing");
-            setOnClickListener(clicker);
-        }
-    }
-
-    @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-
-        // Record to the external cache directory for visibility
-        mFileName = getExternalCacheDir().getAbsolutePath();
-        mFileName += "/audiorecordtest.3gp";
-
-        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
-
-        LinearLayout ll = new LinearLayout(this);
-        mRecordButton = new RecordButton(this);
-        ll.addView(mRecordButton,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        0));
-        mPlayButton = new PlayButton(this);
-        ll.addView(mPlayButton,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        0));
-        setContentView(ll);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mRecorder != null) {
-            mRecorder.release();
-            mRecorder = null;
-        }
-
-        if (mPlayer != null) {
-            mPlayer.release();
-            mPlayer = null;
-        }
-    }
-}
-/*public class MainActivity extends AppCompatActivity {
     private String TAG = "BluetoothRecord";
+    boolean isRecording=false;
+    private short[] mBuffer;
+    AudioRecord mARecorder;
+    AudioTrack mATracker;
+    private MediaRecorder mMRecorder = null;
     private static String mFileName = null;
     private AudioManager mAudioManager = null;
-    private MediaRecorder mRecorder = null;
     private Button startRecordButton;
     private Button stopRecordButton;
+    private Button MicRecordButton;
     // Requesting permission to RECORD_AUDIO
     private boolean permissionToRecordAccepted = false;
     private String [] permissions =
@@ -250,12 +97,82 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
+        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFileName += "/record.pcm";
         mAudioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-
         startRecordButton = (Button)findViewById(R.id.button1);
         stopRecordButton = (Button)findViewById(R.id.button2);
+        MicRecordButton = (Button)findViewById(R.id.button3);
         startRecordButton.setText("start record");
         stopRecordButton.setText("stop record");
+        MicRecordButton.setText("mic start");
+
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int state = intent.getIntExtra(AudioManager.EXTRA_SCO_AUDIO_STATE, -1);
+
+                if (AudioManager.SCO_AUDIO_STATE_CONNECTED == state) {
+                    Log.i(TAG, "AudioManager.SCO_AUDIO_STATE_CONNECTED");
+                    mAudioManager.setSpeakerphoneOn(false);
+                    mAudioManager.setBluetoothScoOn(true);  //打开SCO
+                    Log.i(TAG, "Routing:" + mAudioManager.isBluetoothScoOn());
+                    mAudioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
+                    //mRecorder.start();//开始录音
+                } else if(AudioManager.SCO_AUDIO_STATE_DISCONNECTED == state){
+                    Log.i(TAG, "AudioManager.SCO_AUDIO_STATE_DISCONNECTED");
+                    unregisterReceiver(this);  //别遗漏
+                } else if(AudioManager.SCO_AUDIO_STATE_CONNECTING == state) {
+                    Log.i(TAG, "AudioManager.SCO_AUDIO_STATE_CONNECTING");
+                } else{//等待一秒后再尝试启动SCO
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    mAudioManager.startBluetoothSco();
+                    Log.i(TAG, "再次startBluetoothSco()");
+
+                }
+            }
+        }, new IntentFilter(AudioManager.ACTION_SCO_AUDIO_STATE_UPDATED));
+
+        MicRecordButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                if(isRecording)
+                {
+                    MicRecordButton.setText("mic start");
+                    isRecording=false;
+                    mARecorder.stop();
+                    mATracker.flush();
+                    mATracker.stop();
+                    mATracker.release();
+                    if (mAudioManager.isBluetoothScoOn()) {
+                        Log.i(TAG, "stop sco recording");
+                        mAudioManager.setBluetoothScoOn(false);
+                        mAudioManager.stopBluetoothSco();
+                        mAudioManager.setSpeakerphoneOn(true);
+                    }
+                } else{
+                    initRecorder();
+                    if (!mAudioManager.isBluetoothScoAvailableOffCall()) {
+                        Log.i(TAG, "system not support bluetooth record");
+                        return;
+                    }
+                    Log.i(TAG, "system support bluetooth record");
+                    mAudioManager.stopBluetoothSco();
+                    mAudioManager.startBluetoothSco();//蓝牙录音的关键，启动SCO连接，耳机话筒才起作用
+                    MicRecordButton.setText("mic stop");
+                    isRecording=true;
+                    mARecorder.startRecording();
+                    mATracker.play();
+                    Log.i(TAG, mFileName);
+                    startBufferedWrite(new File(mFileName));
+                }
+            }
+        });
         startRecordButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -270,6 +187,7 @@ public class MainActivity extends AppCompatActivity {
                 stopRecording();
             }
         });
+
     }
 
     @Override
@@ -294,25 +212,22 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }    //record
     private void startRecording(){
-
-        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
-        mFileName += "/record.3gp";
-        mRecorder = new MediaRecorder();
-        mRecorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
-        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mRecorder.setOutputFile(mFileName);
-        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        //mRecorder = new MediaRecorder();
+        //mRecorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
+        //mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        //mRecorder.setOutputFile(mFileName);
+        //mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
         Context context = getApplicationContext();
         CharSequence text = "Hello toast!";
         int duration = Toast.LENGTH_SHORT;
-        try {
-            mRecorder.prepare();
-        } catch (Exception e) {
-            // TODO: handle exception
-            Toast toast = Toast.makeText(context, "prepare() failed!", duration);
-            toast.show();
-            Log.i(TAG, "prepare() failed!");
-        }
+        //try {
+        //    mRecorder.prepare();
+        //} catch (Exception e) {
+        //    // TODO: handle exception
+        //    Toast toast = Toast.makeText(context, "prepare() failed!", duration);
+        //    toast.show();
+        //    Log.i(TAG, "prepare() failed!");
+        //}
         if (!mAudioManager.isBluetoothScoAvailableOffCall()) {
             Toast toast = Toast.makeText(context, "system not support bluetooth record", duration);
             toast.show();
@@ -322,42 +237,83 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, "system support bluetooth record");
         mAudioManager.stopBluetoothSco();
         mAudioManager.startBluetoothSco();//蓝牙录音的关键，启动SCO连接，耳机话筒才起作用
-
-        registerReceiver(new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                int state = intent.getIntExtra(AudioManager.EXTRA_SCO_AUDIO_STATE, -1);
-
-                if (AudioManager.SCO_AUDIO_STATE_CONNECTED == state) {
-                    Log.i(TAG, "AudioManager.SCO_AUDIO_STATE_CONNECTED");
-                    mAudioManager.setBluetoothScoOn(true);  //打开SCO
-                    Log.i(TAG, "Routing:" + mAudioManager.isBluetoothScoOn());
-                    mAudioManager.setMode(AudioManager.STREAM_MUSIC);
-                    mRecorder.start();//开始录音
-                    unregisterReceiver(this);  //别遗漏
-               }else {//等待一秒后再尝试启动SCO
-                    try {
-                    Thread.sleep(10000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                    mAudioManager.startBluetoothSco();
-                    Log.i(TAG, "再次startBluetoothSco()");
-
-                }
-            }
-        }, new IntentFilter(AudioManager.ACTION_SCO_AUDIO_STATE_CHANGED));
     }
 
 
     private void stopRecording(){
-        //mAudioManager.stopBluetoothSco();
-        mRecorder.stop();
-        mRecorder.release();
-        mRecorder = null;
         if (mAudioManager.isBluetoothScoOn()) {
+            Log.i(TAG, "stop sco recording");
             mAudioManager.setBluetoothScoOn(false);
             mAudioManager.stopBluetoothSco();
+            mAudioManager.setSpeakerphoneOn(true);
         }
+        /*
+                mAudioManager.stopBluetoothSco();
+                if ( mRecorder == null)
+                return;
+                mRecorder.stop();
+                mRecorder.reset();
+                mRecorder.release();
+                mRecorder = null;
+                */
     }
-}*/
+    private void startBufferedWrite(final File file) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                DataOutputStream output = null;
+                try {
+                    output = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(file)));
+                    while (isRecording) {
+                        int readSize = mARecorder.read(mBuffer, 0, mBuffer.length);
+                        mATracker.write(mBuffer, 0, mBuffer.length);
+                        for (int i = 0; i < readSize; i++) {
+                            output.writeShort(mBuffer[i]);
+                        }
+                    }
+                } catch (IOException e) {
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                } finally {
+                    if (output != null) {
+                        try {
+                            output.flush();
+                        } catch (IOException e) {
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        } finally {
+                            try {
+                                Log.i(TAG, "close file");
+                                output.close();
+                            } catch (IOException e) {
+                                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                }
+            }
+        }).start();
+    }
+    private void initRecorder() {
+        int bufferSize = AudioRecord.getMinBufferSize(8000,
+                AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        mBuffer = new short[bufferSize];
+
+        mARecorder = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, 8000,
+                AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT,
+                bufferSize);
+
+        mATracker = new AudioTrack(AudioManager.MODE_IN_COMMUNICATION, 8000,
+                AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, bufferSize, AudioTrack.MODE_STREAM);
+        mATracker.setPlaybackRate(8000);
+    }
+    private void DisplayToast(String msg)
+    {
+         Toast.makeText(getBaseContext(), msg,
+                 Toast.LENGTH_SHORT).show();
+    }    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+        mARecorder.release();
+    }
+}
